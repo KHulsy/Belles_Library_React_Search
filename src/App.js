bbc08f3ee7e26c95b7cbcae163212c1055e4c8f1
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-08-22 12:05:22 -0700

    Update App.js
    Navbar, etc.

--- src/App.js
+++ src/App.js
@@ -23,3 +23,4 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
